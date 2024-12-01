dbbd198d5b2045a712f16274caac11c9c84a864c
nolen777 <dan@danielcrosby.net>
2024-12-01 10:50:09 -0800

    initial setup and day1part1 (#1)

---
+++ src/main/scala/Day1/Part1/Day1Part1Main.scala
@@ -0,0 +1,23 @@
+package Day1.Part1
+
+import scala.io.Source
+import scala.util.Using
+
+@main def Day1Part1 = {
+  val (leftList, rightList) =
+    Using(Source.fromFile("src/main/scala/Day1/Part1/input.txt")) { src =>
+      val lists = src
+        .getLines()
+        .map(_.split(" {3}").map(_.toInt).toArray)
+        .toArray
+        .transpose
+      (lists(0), lists(1))
+    }.get
+
+  println(
+    leftList.sorted
+      .zip(rightList.sorted)
+      .map { case (left, right) => Math.abs(left - right) }
+      .sum
+  )
+}
