714db71329587a7c226b4078c1da9609c93b6764
Dan Crosby <dan@danielcrosby.net>
2024-12-17 21:25:39 -0800

    part1

--- src/main/go/Day18/day18.go
+++ src/main/go/Day18/day18.go
@@ -5,9 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func parseLines(fileName string) []string {
+type Coords struct {
+	x int
+	y int
+}
+
+type Maze [][]rune
+
+const (
+	Empty     = '.'
+	Corrupted = '#'
+	Path      = 'O'
+)
+
+func parseCoords(fileName string) []Coords {
 	file, err := os.Open(fileName)
 
 	if err != nil {
@@ -16,22 +31,120 @@ func parseLines(fileName string) []string {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	lines := []string{}
+	coords := []Coords{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		lines = append(lines, line)
+		nums := strings.Split(line, ",")
+		x, _ := strconv.Atoi(nums[0])
+		y, _ := strconv.Atoi(nums[1])
+		coords = append(coords, Coords{x: x, y: y})
 	}
-	return lines
+	return coords
+}
+
+func SetupMaze(size int, corruption []Coords) Maze {
+	maze := make([][]rune, 0, size)
+	for i := 0; i < size; i++ {
+		line := make([]rune, 0, size)
+		for j := 0; j < size; j++ {
+			line = append(line, Empty)
+		}
+		maze = append(maze, line)
+	}
+
+	for _, c := range corruption {
+		maze[c.x][c.y] = Corrupted
+	}
+	return maze
+}
+
+func PrintMaze(m Maze) {
+	size := len(m)
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			fmt.Print(string(m[x][y]))
+		}
+		fmt.Println("")
+	}
+}
+
+const MaxUint = ^uint(0)
+const MaxInt = int(MaxUint >> 1)
+
+func CalculateCosts(from Coords, m Maze) [][]int {
+	size := len(m)
+
+	isValid := func(x int, y int) bool {
+		return x >= 0 && y >= 0 && x < size && y < size
+	}
+
+	costs := make([][]int, 0, size)
+	for i := 0; i < size; i++ {
+		line := make([]int, 0, size)
+		for j := 0; j < size; j++ {
+			line = append(line, MaxInt)
+		}
+		costs = append(costs, line)
+	}
+
+	costs[from.x][from.y] = 0
+
+	changes := 1
+	// Go through every tile repeatedly. On each tile, see if we've identified
+	// a cheaper path to it
+	for changes > 0 {
+		changes = 0
+		for y := 0; y < size; y++ {
+			for x := 0; x < size; x++ {
+				if m[x][y] == Corrupted {
+					continue
+				}
+
+				if isValid(x-1, y) {
+					if costs[x-1][y] < MaxInt && costs[x-1][y]+1 < costs[x][y] {
+						changes++
+						costs[x][y] = costs[x-1][y] + 1
+					}
+				}
+				if isValid(x+1, y) {
+					if costs[x+1][y] < MaxInt && costs[x+1][y]+1 < costs[x][y] {
+						changes++
+						costs[x][y] = costs[x+1][y] + 1
+					}
+				}
+				if isValid(x, y-1) {
+					if costs[x][y-1] < MaxInt && costs[x][y-1]+1 < costs[x][y] {
+						changes++
+						costs[x][y] = costs[x][y-1] + 1
+					}
+				}
+				if isValid(x, y+1) {
+					if costs[x][y+1] < MaxInt && costs[x][y+1]+1 < costs[x][y] {
+						changes++
+						costs[x][y] = costs[x][y+1] + 1
+					}
+				}
+			}
+		}
+	}
+
+	return costs
 }
 
 func part1() {
-	lines := parseLines("resources/Day18/sample.txt")
-	fmt.Println(lines)
+	coords := parseCoords("resources/Day18/sample.txt")
+	fmt.Println(coords)
+
+	maze := SetupMaze(7, coords[:12])
+	PrintMaze(maze)
+
+	costs := CalculateCosts(Coords{x: 0, y: 0}, maze)
+	fmt.Println("Steps: ", costs[6][6])
 }
 
 func part2() {
-	lines := parseLines("resources/Day18/sample.txt")
+	lines := parseCoords("resources/Day18/sample.txt")
 	_ = lines
 }
 
