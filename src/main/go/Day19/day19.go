e92a8119f883380016849267e7a98a1c60059fea
Dan Crosby <dan@danielcrosby.net>
2024-12-18 21:20:39 -0800

    part 1

--- src/main/go/Day19/day19.go
+++ src/main/go/Day19/day19.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
+	"strings"
 )
 
-func parseLines(fileName string) []string {
+func parseTowelsAndPatterns(fileName string) ([]string, []string) {
 	file, err := os.Open(fileName)
 
 	if err != nil {
@@ -16,23 +18,66 @@ func parseLines(fileName string) []string {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	lines := []string{}
 
+	// first parse available towels
+	scanner.Scan()
+	line := scanner.Text()
+	towels := strings.Split(line, ", ")
+
+	scanner.Scan()
+	line = scanner.Text()
+
+	patterns := []string{}
 	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
+		pattern := scanner.Text()
+		patterns = append(patterns, pattern)
 	}
-	return lines
+	return towels, patterns
+}
+
+func recursiveFindTowelsForPattern(allTowels []string, pattern string, usedTowels []string) ([]string, bool) {
+	if pattern == "" {
+		return usedTowels, true
+	}
+
+	for _, towel := range allTowels {
+		if strings.HasPrefix(pattern, towel) {
+			newUsed := slices.Clone(usedTowels)
+			newUsed = append(newUsed, towel)
+
+			result, ok := recursiveFindTowelsForPattern(allTowels, pattern[len(towel):], newUsed)
+
+			if ok {
+				return result, true
+			}
+		}
+	}
+
+	return usedTowels, false
 }
 
 func part1() {
-	lines := parseLines("resources/Day19/sample.txt")
-	fmt.Println(lines)
+	towels, patterns := parseTowelsAndPatterns("resources/Day19/sample.txt")
+
+	successCount := 0
+	for _, pattern := range patterns {
+		usedTowels, ok := recursiveFindTowelsForPattern(towels, pattern, []string{})
+
+		if ok {
+			successCount++
+			fmt.Println("Success for ", pattern, " with ", usedTowels)
+		} else {
+			fmt.Println(pattern, " is impossible")
+		}
+	}
+
+	fmt.Println("Success count: ", successCount)
 }
 
 func part2() {
-	lines := parseLines("resources/Day19/sample.txt")
-	_ = lines
+	towels, patterns := parseTowelsAndPatterns("resources/Day19/sample.txt")
+	_ = towels
+	_ = patterns
 }
 
 func main() {
