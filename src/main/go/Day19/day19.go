0de690c08afdc84c571c3d03351f26211a6ae257
Dan Crosby <dan@danielcrosby.net>
2024-12-18 21:44:31 -0800

    search tree

--- src/main/go/Day19/day19.go
+++ src/main/go/Day19/day19.go
@@ -35,17 +35,35 @@ func parseTowelsAndPatterns(fileName string) ([]string, []string) {
 	return towels, patterns
 }
 
-func recursiveFindTowelsForPattern(allTowels []string, pattern string, usedTowels []string) ([]string, bool) {
+type TowelTree struct {
+	present bool
+
+	sub [5]*TowelTree
+}
+
+func (tt TowelTree) contains(t string) bool {
+	if t == "" {
+		return tt.present
+	}
+	sub := tt.sub[index(rune(t[0]))]
+	if sub == nil {
+		return false
+	}
+	return sub.contains(t[1:])
+}
+
+func recursiveFindTowelsForPattern(allTowels TowelTree, pattern string, usedTowels []string) ([]string, bool) {
 	if pattern == "" {
 		return usedTowels, true
 	}
 
-	for _, towel := range allTowels {
-		if strings.HasPrefix(pattern, towel) {
+	for i := 1; i <= len(pattern); i++ {
+		substr := pattern[:i]
+		if allTowels.contains(substr) {
 			newUsed := slices.Clone(usedTowels)
-			newUsed = append(newUsed, towel)
+			newUsed = append(newUsed, substr)
 
-			result, ok := recursiveFindTowelsForPattern(allTowels, pattern[len(towel):], newUsed)
+			result, ok := recursiveFindTowelsForPattern(allTowels, pattern[i:], newUsed)
 
 			if ok {
 				return result, true
@@ -56,12 +74,50 @@ func recursiveFindTowelsForPattern(allTowels []string, pattern string, usedTowel
 	return usedTowels, false
 }
 
+func index(color rune) int {
+	switch color {
+	case 'w':
+		return 0
+	case 'u':
+		return 1
+	case 'b':
+		return 2
+	case 'r':
+		return 3
+	case 'g':
+		return 4
+	}
+	log.Fatal("Unknown color", color)
+	return -1
+}
+
+func makeTowelTree(towels []string) TowelTree {
+	root := TowelTree{present: false}
+
+	for _, towel := range towels {
+		tt := &root
+
+		for _, r := range towel {
+			idx := index(r)
+
+			if tt.sub[idx] == nil {
+				tt.sub[idx] = &TowelTree{}
+			}
+			tt = tt.sub[idx]
+		}
+		tt.present = true
+	}
+	return root
+}
+
 func part1() {
 	towels, patterns := parseTowelsAndPatterns("resources/Day19/sample.txt")
 
+	towelTree := makeTowelTree(towels)
+
 	successCount := 0
 	for _, pattern := range patterns {
-		usedTowels, ok := recursiveFindTowelsForPattern(towels, pattern, []string{})
+		usedTowels, ok := recursiveFindTowelsForPattern(towelTree, pattern, []string{})
 
 		if ok {
 			successCount++
