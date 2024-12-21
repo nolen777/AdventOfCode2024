0e92a1b2c8b7d467f7eea319a59145687f14dc34
nolen777 <dan@danielcrosby.net>
2024-12-21 14:39:22 -0800

    21cleanup (#28)
    * still ugly
    
    * lots of code removed
    
    * more cleanup

--- src/main/go/Day21/day21.go
+++ src/main/go/Day21/day21.go
@@ -33,7 +33,6 @@ const (
 	Left  = '<'
 	Down  = 'v'
 	Right = '>'
-	Panic = byte(255)
 )
 
 type Coords struct {
@@ -44,196 +43,86 @@ type Coords struct {
 type Pad struct {
 	rowCount    int
 	columnCount int
-	keys        [][]byte
+	keyCoords   map[byte]Coords
+	panicCoords Coords
 }
 
-type NumberPad Pad
-type DirectionPad Pad
+var numberPad = Pad{rowCount: 4, columnCount: 3, keyCoords: map[byte]Coords{
+	'7': {0, 0},
+	'8': {0, 1},
+	'9': {0, 2},
+	'4': {1, 0},
+	'5': {1, 1},
+	'6': {1, 2},
+	'1': {2, 0},
+	'2': {2, 1},
+	'3': {2, 2},
+	'0': {3, 1},
+	A:   {3, 2},
+}, panicCoords: Coords{3, 0}}
 
-func (np NumberPad) CoordsOf(b byte) Coords {
-	switch b {
-	case '7':
-		return Coords{0, 0}
-	case '8':
-		return Coords{0, 1}
-	case '9':
-		return Coords{0, 2}
-	case '4':
-		return Coords{1, 0}
-	case '5':
-		return Coords{1, 1}
-	case '6':
-		return Coords{1, 2}
-	case '1':
-		return Coords{2, 0}
-	case '2':
-		return Coords{2, 1}
-	case '3':
-		return Coords{2, 2}
-	case Panic:
-		return Coords{3, 0}
-	case '0':
-		return Coords{3, 1}
-	case A:
-		return Coords{3, 2}
+var directionPad = Pad{rowCount: 2, columnCount: 3, keyCoords: map[byte]Coords{
+	Up:    {0, 1},
+	A:     {0, 2},
+	Left:  {1, 0},
+	Down:  {1, 1},
+	Right: {1, 2}}, panicCoords: Coords{0, 0}}
+
+func (p Pad) CoordDirections(from Coords, to Coords, ms string) []string {
+	if from == to {
+		return []string{ms}
+	}
+	if from == p.panicCoords {
+		log.Fatal("Can't start at Panic")
+	}
+	if to == p.panicCoords {
+		log.Fatal("Can't go to Panic")
 	}
 
-	log.Fatal("Bad numberpad byte ", string(rune(b)))
-	return Coords{-1, -1}
-}
+	rDiff := to.row - from.row
+	cDiff := to.column - from.column
 
-func (dp DirectionPad) CoordsOf(b byte) Coords {
-	switch b {
-	case Panic:
-		return Coords{0, 0}
-	case Up:
-		return Coords{0, 1}
-	case A:
-		return Coords{0, 2}
-	case Left:
-		return Coords{1, 0}
-	case Down:
-		return Coords{1, 1}
-	case Right:
-		return Coords{1, 2}
+	directions := []string{}
+
+	if rDiff > 0 {
+		next := Coords{from.row + 1, from.column}
+		if next != p.panicCoords {
+			added := p.CoordDirections(next, to, ms+string(Down))
+			directions = append(directions, added...)
+		}
 	}
-
-	log.Fatal("Bad directionpad byte ", string(rune(b)))
-	return Coords{-1, -1}
-}
-
-func (p Pad) CoordsOf(b byte) Coords {
-	for rIdx, row := range p.keys {
-		for cIdx, entry := range row {
-			if entry == b {
-				return Coords{rIdx, cIdx}
-			}
+	if rDiff < 0 {
+		next := Coords{from.row - 1, from.column}
+		if next != p.panicCoords {
+			added := p.CoordDirections(next, to, ms+string(Up))
+			directions = append(directions, added...)
+		}
+	}
+	if cDiff > 0 {
+		next := Coords{from.row, from.column + 1}
+		if next != p.panicCoords {
+			added := p.CoordDirections(next, to, ms+string(Right))
+			directions = append(directions, added...)
+		}
+	}
+	if cDiff < 0 {
+		next := Coords{from.row, from.column - 1}
+		if next != p.panicCoords {
+			added := p.CoordDirections(next, to, ms+string(Left))
+			directions = append(directions, added...)
 		}
 	}
 
-	log.Fatal(b, "not found in", p)
-	return Coords{-1, -1}
-}
-
-func CreateNumberPad() NumberPad {
-	pad := NumberPad{rowCount: 4, columnCount: 3, keys: [][]byte{}}
-
-	row0 := []byte{'7', '8', '9'}
-	row1 := []byte{'4', '5', '6'}
-	row2 := []byte{'1', '2', '3'}
-	row3 := []byte{Panic, '0', A}
-
-	pad.keys = append(pad.keys, row0, row1, row2, row3)
-	return pad
-}
-
-func CreateDirectionPad() DirectionPad {
-	pad := DirectionPad{rowCount: 2, columnCount: 3, keys: [][]byte{}}
-
-	row0 := []byte{Panic, Up, A}
-	row1 := []byte{Left, Down, Right}
-
-	pad.keys = append(pad.keys, row0, row1)
-
-	return pad
-}
-
-func (p NumberPad) CoordDirections(from Coords, to Coords, ms string) []string {
-	if from == to {
-		return []string{ms}
-	}
-
-	rDiff := to.row - from.row
-	cDiff := to.column - from.column
-
-	directions := []string{}
-
-	if rDiff > 0 && p.keys[from.row+1][from.column] != Panic {
-		added := p.CoordDirections(Coords{from.row + 1, from.column}, to, ms+string(Down))
-		directions = append(directions, added...)
-	}
-	if rDiff < 0 && p.keys[from.row-1][from.column] != Panic {
-		added := p.CoordDirections(Coords{from.row - 1, from.column}, to, ms+string(Up))
-		directions = append(directions, added...)
-	}
-	if cDiff > 0 && p.keys[from.row][from.column+1] != Panic {
-		added := p.CoordDirections(Coords{from.row, from.column + 1}, to, ms+string(Right))
-		directions = append(directions, added...)
-	}
-	if cDiff < 0 && p.keys[from.row][from.column-1] != Panic {
-		added := p.CoordDirections(Coords{from.row, from.column - 1}, to, ms+string(Left))
-		directions = append(directions, added...)
-	}
-
 	return directions
 }
 
-func (dp DirectionPad) CoordDirections(from Coords, to Coords, ms string) []string {
-	if from == to {
-		return []string{ms}
-	}
-
-	rDiff := to.row - from.row
-	cDiff := to.column - from.column
-
-	directions := []string{}
-
-	if rDiff > 0 && dp.keys[from.row+1][from.column] != Panic {
-		added := dp.CoordDirections(Coords{from.row + 1, from.column}, to, ms+string(Down))
-		directions = append(directions, added...)
-	}
-	if rDiff < 0 && dp.keys[from.row-1][from.column] != Panic {
-		added := dp.CoordDirections(Coords{from.row - 1, from.column}, to, ms+string(Up))
-		directions = append(directions, added...)
-	}
-	if cDiff > 0 && dp.keys[from.row][from.column+1] != Panic {
-		added := dp.CoordDirections(Coords{from.row, from.column + 1}, to, ms+string(Right))
-		directions = append(directions, added...)
-	}
-	if cDiff < 0 && dp.keys[from.row][from.column-1] != Panic {
-		added := dp.CoordDirections(Coords{from.row, from.column - 1}, to, ms+string(Left))
-		directions = append(directions, added...)
-	}
-
-	return directions
-}
-
-//func (p Pad) Directions(from byte, to byte, ms string) []string {
-//	fC := p.CoordsOf(from)
-//	tC := p.CoordsOf(to)
-//
-//	return p.CoordDirections(fC, tC, ms)
-//}
-
-func (np NumberPad) FindSequences(ms string) []string {
-	padPosition := np.CoordsOf(A)
+func (np Pad) FindSequences(ms string) []string {
+	padPosition := np.keyCoords[A]
 	allSeqs := []string{""}
 
 	for _, button := range ms {
-		newCoords := np.CoordsOf(byte(button))
+		newCoords := np.keyCoords[byte(button)]
 		nextSeqs := np.CoordDirections(padPosition, newCoords, "")
-
-		extended := make([]string, 0, len(allSeqs)*len(nextSeqs))
-		for _, e := range allSeqs {
-			for _, ns := range nextSeqs {
-				extended = append(extended, e+ns+string(A))
-			}
-		}
-		allSeqs = extended
-
-		padPosition = newCoords
-	}
-
-	return allSeqs
-}
-
-func (dp DirectionPad) FindSequences(ms string) []string {
-	padPosition := dp.CoordsOf(A)
-	allSeqs := []string{""}
-
-	for _, button := range ms {
-		newCoords := dp.CoordsOf(byte(button))
-		nextSeqs := dp.CoordDirections(padPosition, newCoords, "")
 
 		extended := make([]string, 0, len(allSeqs)*len(nextSeqs))
 		for _, e := range allSeqs {
@@ -263,7 +152,7 @@ var costCache = map[CostCacheKey]int{}
 const MaxUint = ^uint(0)
 const MaxInt = int(MaxUint >> 1)
 
-func (dp DirectionPad) CostFor(seq string, prefix string, remainingDepth int) int {
+func (dp Pad) CostFor(seq string, prefix string, remainingDepth int) int {
 	superKey := CostCacheKey{remainingDepth, seq}
 	if cachedCost, ok := costCache[superKey]; ok {
 		return cachedCost
@@ -311,18 +200,14 @@ func calculate(depth int) {
 	lines := parseCodes("resources/Day21/input.txt")
 	fmt.Println(lines)
 
-	numPad := CreateNumberPad()
-	dirPad := CreateDirectionPad()
-
 	totalComplexity := 0
 	for _, line := range lines {
 		minCost := MaxInt
 
-		numPadSeqs := numPad.FindSequences(string(line))
+		numPadSeqs := numberPad.FindSequences(line)
 
-		_ = dirPad
 		for _, numPadSeq := range numPadSeqs {
-			cost := dirPad.CostFor(numPadSeq, "", depth)
+			cost := directionPad.CostFor(numPadSeq, "", depth)
 			if cost < minCost {
 				minCost = cost
 			}
