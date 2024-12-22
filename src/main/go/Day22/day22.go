085dfd7d793bdf2510e0bd3ec383e7c60b21d381
Dan Crosby <dan@danielcrosby.net>
2024-12-21 22:06:17 -0800

    part 1

--- src/main/go/Day22/day22.go
+++ src/main/go/Day22/day22.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
-func parseLines(fileName string) []string {
+func parseSecrets(fileName string) []int {
 	file, err := os.Open(fileName)
 
 	if err != nil {
@@ -16,22 +17,54 @@ func parseLines(fileName string) []string {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	lines := []string{}
+	nums := []int{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		lines = append(lines, line)
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal("Invalid input")
+		}
+		nums = append(nums, i)
 	}
-	return lines
+	return nums
+}
+
+func mix(secret int, value int) int {
+	return secret ^ value
+}
+
+func prune(secret int) int {
+	return secret % 16777216
+}
+
+func evolve(secret int) int {
+	secret = prune(mix(secret, secret<<6))
+	secret = prune(mix(secret, secret>>5))
+	secret = prune(mix(secret, secret<<11))
+
+	return secret
 }
 
 func part1() {
-	lines := parseLines("resources/Day22/sample.txt")
-	fmt.Println(lines)
+	secrets := parseSecrets("resources/Day22/sample.txt")
+	fmt.Println(secrets)
+
+	sum := 0
+	for _, secret := range secrets {
+		newSecret := secret
+		for i := 0; i < 2000; i++ {
+			newSecret = evolve(newSecret)
+		}
+		fmt.Println(secret, ": ", newSecret)
+		sum += newSecret
+	}
+
+	fmt.Println("Sum: ", sum)
 }
 
 func part2() {
-	lines := parseLines("resources/Day2/sample.txt")
+	lines := parseSecrets("resources/Day2/sample.txt")
 	_ = lines
 }
 
