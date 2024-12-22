357b881f95ef12db0095f5deb7a5d93403d83484
Dan Crosby <dan@danielcrosby.net>
2024-12-22 12:54:10 -0800

    part2 works

--- src/main/go/Day22/day22.go
+++ src/main/go/Day22/day22.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 )
 
@@ -63,11 +64,66 @@ func part1() {
 	fmt.Println("Sum: ", sum)
 }
 
+type Seq4 struct {
+	a, b, c, d int
+}
+
+func (s4 Seq4) sum() int {
+	return s4.a + s4.b + s4.c + s4.d
+}
+
+func (s4 Seq4) slide(newVal int) Seq4 {
+	return Seq4{s4.b, s4.c, s4.d, newVal}
+}
+
 func part2() {
-	lines := parseSecrets("resources/Day2/sample.txt")
-	_ = lines
+	secrets := parseSecrets("resources/Day22/input.txt")
+
+	sequenceSums := map[Seq4]int{}
+	var expected = Seq4{-2, 1, -1, 3}
+	_ = expected
+
+	for _, secret := range secrets {
+		newSecret := secret
+		lastPrice := newSecret % 10
+
+		thisSeqs := map[Seq4]bool{}
+
+		last4 := Seq4{}
+
+		for i := 0; i < 2000; i++ {
+			newSecret = evolve(newSecret)
+			newPrice := newSecret % 10
+			priceChange := newPrice - lastPrice
+
+			last4 = last4.slide(priceChange)
+			if i >= 3 {
+				if !thisSeqs[last4] {
+					if last4 == expected {
+						fmt.Println("here we are")
+					}
+					thisSeqs[last4] = true
+					sequenceSums[last4] += newPrice
+				}
+			}
+
+			lastPrice = newPrice
+		}
+	}
+
+	type kv struct {
+		k Seq4
+		v int
+	}
+	kvs := make([]kv, 0, len(sequenceSums))
+	for k, v := range sequenceSums {
+		kvs = append(kvs, kv{k, v})
+	}
+	slices.SortFunc(kvs, func(kv1 kv, kv2 kv) int { return kv2.v - kv1.v })
+
+	fmt.Println(kvs[0])
 }
 
 func main() {
-	part1()
+	part2()
 }
